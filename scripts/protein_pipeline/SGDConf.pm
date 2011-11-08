b80eaf9f17ac205b44307730f2cba8fa07632c6e
arnaud <>
2011-11-08 11:11:27 +0000

    old style gff loader used for loading ncRNA gene models

---
+++ scripts/protein_pipeline/SGDConf.pm
@@ -0,0 +1,68 @@
+package SGDConf;
+
+use strict;
+use vars qw( %SGDConf );
+
+
+# personal data dir (for temporaty & result/error files)
+my $dataDIR  = "/homes/uma/yeast/output";
+
+%SGDConf = (
+		 #location of gff file
+		 SGD_GFF_FILE => '/nfs/panda/production/ensembl_genomes/ensembl-fungi/ensembl-scer/modified-sanger/input_data/saccharomyces_cerevisiae.gff',
+
+		 # database to put sequnece and genes into
+		 SGD_DBNAME => 'saccharomyces_cerevisiae_core_2_54_2a',
+		 SGD_DBHOST => 'mysql-eg-devel-1.ebi.ac.uk',
+		 SGD_DBUSER => 'ensrw',
+		 SGD_DBPASS => 'writ3r',   
+                 SGD_DBPORT => '4126',      
+		 # logic name of analysis object to be assigned to genes
+		 SGD_LOGIC_NAME => 'SGD',
+		 # if want the debug statements in wormbase to ensembl scripts printed
+		 SGD_DEBUG => 1,
+		 # location to write file containing dodgy seq ids
+		 SGD_SEQ_IDS => $dataDIR.'/dodgy_ids',
+		 # location to write ids of genes which don't translate
+		 SGD_NON_TRANSLATE => $dataDIR.'/non_translate',
+		 # location to write ids of genes which don't transform
+     #set to 0 will write entries to input_id_analysis table
+		 SGD_OPERON_LOGIC_NAME => '', #logic names
+		 SGD_RNAI_LOGIC_NAME => '',  # for simple features to be 
+		 SGD_EXPR_LOGIC_NAME => '', # parsed out of the gff
+		 SGD_PSEUDO_LOGIC_NAME => 'pseudogene',
+                 SGD_CLONE_SYSTEM_NAME => 'Clone',
+                 SGD_CHROMOSOME_SYSTEM_NAME => 'chromosome',
+		);
+
+sub import {
+    my ($callpack) = caller(0); # Name of the calling package
+    my $pack = shift; # Need to move package off @_
+
+    # Get list of variables supplied, or else
+    # all of GeneConf:
+    my @vars = @_ ? @_ : keys( %SGDConf );
+    return unless @vars;
+
+    # Predeclare global variables in calling package
+    eval "package $callpack; use vars qw("
+         . join(' ', map { '$'.$_ } @vars) . ")";
+    die $@ if $@;
+
+
+    foreach (@vars) {
+	if ( defined $SGDConf{ $_ } ) {
+            no strict 'refs';
+	    # Exporter does a similar job to the following
+	    # statement, but for function names, not
+	    # scalar variables:
+	    *{"${callpack}::$_"} = \$SGDConf{ $_ };
+	} else {
+	    die "Error: SGDConf: $_ not known\n";
+	}
+    }
+}
+
+1;
+
+
