7a6c75d1c6d61ffa718866cd4cd859fa0d6c1335
arnaud <>
2011-11-10 10:45:01 +0000

    Added a todo specifying this file should go into the home dir

--- scripts/user-example.env.sh
+++ scripts/user-example.env.sh
@@ -1,3 +1,4 @@
+# Todo: Move this file in your home directory
 # Todo: Assign the DB_PASS variable
 
 # Define the database connectivity
@@ -5,22 +6,22 @@
 #export DB_HOST=mysql-eg-staging-1.ebi.ac.uk
 #export DB_PORT=4160
 #export DB_USER=ensrw
-#export DB_PASS=
+#export DB_PASS=xxxx
 
 # export DB_HOST=mysql-eg-devel-2.ebi.ac.uk
 # export DB_PORT=4207
 # export DB_USER=ensrw
-# export DB_PASS=
+# export DB_PASS=xxxx
 
 # export DB_HOST=mysql-eg-devel-3.ebi.ac.uk
 # export DB_PORT=4208
 # export DB_USER=ensrw
-# export DB_PASS=
+# export DB_PASS=xxxx
 
  export DB_HOST=mysql-cluster-eg-prod-1.ebi.ac.uk
  export DB_PORT=4238
  export DB_USER=ensrw
- export DB_PASS=
+ export DB_PASS=xxxx
 
 #CONFIG_DIR=/nfs/panda/ensemblgenomes/apis/ensembl/config/head/plasmodia/PC_01_11
 CONFIG_DIR=/nfs/panda/ensemblgenomes/apis/proteomes/ensembl_genomes/EG-pipelines/config/scerevisiae
