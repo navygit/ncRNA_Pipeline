1152e37cc8c59b7c2eb51ea340d3778eb1e281ee
jallen <>
2012-09-20 13:07:57 +0000

    Removed redundant scripts, and merged in some of the tidying from 'final_steps-tidy.sh'

--- scripts/final_steps/final_steps.sh
+++ scripts/final_steps/final_steps.sh
@@ -1,35 +1,43 @@
 #!/bin/sh
 
-# Todo: Add -snpstats to seq_region_stats.pl run when it is Scer or Gzeae
-# or for whatever species which has a variation database
-
-# alternative translation loading ???
-
-PERL_PATH=/nfs/panda/ensemblgenomes/perl/
-#EMBOSS=/sw/arch/pkg/EMBOSS-5.0.0/
-EMBOSS=/nfs/production/panda/ensemblgenomes/external/EMBOSS-6.4.0_x86_64-Linux/
-CODE=/nfs/panda/ensemblgenomes/production/final_steps
-ENSEMBL_PATH=${CODE}/ensembl-head
-BIOPERL_PATH=/nfs/panda/ensemblgenomes/apis/bioperl/stable/
-
-if [ $# != 1 ]
+if [ $# -lt 1 ]
 then
     echo "Wrong number of command line arguments"
-    echo "sh ./final_steps.sh puccinia_graministritici_core_7_60_1a"
+    echo "sh final_steps.sh <species_name>_core_XX_YY_Z [<use_work_dir>]"
+    echo "e.g. brassica_rapa_core_12_65_1"
     exit 1
 fi
 
 DB_NAME=$1
+WORKDIR=$2
 
-# One file specific for each user, based on ${USER}
+# By default the script uses the checkout on nfs, so there are effectively no
+# prerequisites. If you already have a working directory defined, however, you
+# can just point to that instead, by passing a true value as a second parameter.
+if [ $WORKDIR ]
+then
+	ENSEMBL_PATH=$EG_BASEDIR/release$EG_VERSION/ensembl_checkouts/ensembl
+else
+	PERL_PATH=/nfs/panda/ensemblgenomes/perl/
+	CODE=/nfs/panda/ensemblgenomes/production/final_steps
+	ENSEMBL_PATH=${CODE}/ensembl-head
+	BIOPERL_PATH=/nfs/panda/ensemblgenomes/apis/bioperl/stable/
+	export PERL5LIB=${ENSEMBL_PATH}/modules:${CODE}/ensembl-variation-head/modules:${BIOPERL_PATH}:$PERL5LIB
+	export PATH=${PERL_PATH}/perl/perlbrew/perls/5.14.2/bin:$PATH
+fi
+
+
+# Configure the MySQL connection
+
+# We expect one file specific for each user, based on ${HOME}/${USER}
 
 ENV_FILE="${HOME}/${USER}.env.sh"
 
-echo "Using env file: $ENV_FILE"
+echo "Using env file: '$ENV_FILE'"
 
-if [ ! -f ${ENV_FILE} ] 
-then
-    echo "can't find an environment file, ${ENV_FILE}, that defines the MySQL server parameters"
+if [ ! -f ${ENV_FILE} ]; then
+    echo "can't find an environment file, ${ENV_FILE}"
+    echo "this file defines the MySQL connection parameters"
     echo "create one first"
     exit 1
 fi
@@ -37,140 +45,65 @@ fi
 source $ENV_FILE
 
 echo "database server host, $DB_HOST"
-
-SPECIES=`echo $DB_NAME | perl -ne '$_ =~ /^([^_]+)_([^_]+)_core.+/; $a = $1; $b = $2; print $a . "_" . $b;'`
-echo "SPECIES: $SPECIES"
-
-# e.g. anidulans
-SPECIES_SHORT_NAME=`echo $SPECIES | perl -ne '$_ =~ /^(\w)[^_]+_(\w+)/; $a = $1; $b = $2; print "$a$b";'`
-
-echo "SPECIES_SHORT_NAME: $SPECIES_SHORT_NAME" 
-
 echo ""
 
-export PERL5LIB=${ENSEMBL_PATH}/modules:${CODE}/ensembl-variation-head/modules:${BIOPERL_PATH}:$PERL5LIB
-
-# Add ensgen perl binary path
-export PATH=${PERL_PATH}/perl/perlbrew/perls/5.14.2/bin:$PATH
-
 # Run repeat-types.pl
-
 echo "Running repeat-types.pl"
-echo "perl repeat-types.pl -user $DB_USER -port $DB_PORT -host $DB_HOST -pass $DB_PASS -dbpattern $DB_NAME"
 
-cd ${ENSEMBL_PATH}/misc-scripts/repeats 
-perl repeat-types.pl -user $DB_USER -port $DB_PORT -host $DB_HOST -pass $DB_PASS -dbpattern $DB_NAME
-
-
-# Run the pepstats script
-echo "Running translation_attribs.pl"
-echo "perl translation_attribs.pl -host $DB_HOST -user $DB_USER -port $DB_PORT -pass $DB_PASS -pattern $DB_NAME --binpath=${EMBOSS}"
-
-cd ${ENSEMBL_PATH}/misc-scripts/
-perl translation_attribs.pl -host $DB_HOST -user $DB_USER -port $DB_PORT -pass $DB_PASS -pattern $DB_NAME --binpath=${EMBOSS}
+cd ${ENSEMBL_PATH}/misc-scripts/repeats
+perl repeat-types.pl \
+     -host $DB_HOST -port $DB_PORT \
+     -user $DB_USER -pass $DB_PASS \
+     -dbpattern $DB_NAME
 
 
-# Run the %GC per gene
-
-echo "Running gene_gc.pl"
-
-cd ${ENSEMBL_PATH}/misc-scripts/
-perl gene_gc.pl -host $DB_HOST -user $DB_USER -pass $DB_PASS -port $DB_PORT -pattern $DB_NAME
-
-
-# set canonical transcripts
-
+# Set canonical transcripts
 echo "Running set_canonical_transcripts.pl"
 
 cd ${ENSEMBL_PATH}/misc-scripts/canonical_transcripts
-perl set_canonical_transcripts.pl -dbhost $DB_HOST -dbuser $DB_USER -dbpass $DB_PASS -dbname $DB_NAME -dbport $DB_PORT -coord_system toplevel -write -verbose
+perl set_canonical_transcripts.pl \
+     -dbhost $DB_HOST -dbport $DB_PORT \
+     -dbuser $DB_USER -dbpass $DB_PASS \
+     -dbname $DB_NAME -coord_system toplevel -write
 
 
-# gene_density_calc.pl
-
-echo "Running gene_density_calc.pl"
-echo "perl gene_density_calc.pl -host $DB_HOST -user $DB_USER -pass $DB_PASS -port $DB_PORT -dbname $DB_NAME -mhost mysql-eg-pan-1.ebi.ac.uk -mport 4276 -muser ensro"
-
-cd ${ENSEMBL_PATH}/misc-scripts/density_feature
-perl gene_density_calc.pl -host $DB_HOST -user $DB_USER -pass $DB_PASS -port $DB_PORT -dbname $DB_NAME -mhost mysql-eg-pan-1.ebi.ac.uk -mport 4276 -muser ensro
-
-
-# seq_region_stats.pl
-
-echo ""
-echo "Running seq_region_stats.pl"
-
-echo "perl seq_region_stats.pl -host $DB_HOST -user $DB_USER -pass $DB_PASS -port $DB_PORT -dbname $DB_NAME -stats gene -mhost mysql-eg-pan-1.ebi.ac.uk -mport 4276 -muser ensro"
-
-cd ${ENSEMBL_PATH}/misc-scripts/density_feature
-# old command
-# perl seq_region_stats.pl -host $DB_HOST -user $DB_USER -pass $DB_PASS -port $DB_PORT -dbname $DB_NAME -genestats
-perl seq_region_stats.pl -host $DB_HOST -user $DB_USER -pass $DB_PASS -port $DB_PORT -dbname $DB_NAME -stats gene -mhost mysql-eg-pan-1.ebi.ac.uk -mport 4276 -muser ensro
-# with both snp and gene
-# perl seq_region_stats.pl -host $DB_HOST -user $DB_USER -pass $DB_PASS -port $DB_PORT -dbname $DB_NAME -stats snp -stats gene
-
-
-# repeat_coverage_calc.pl
-
-cd ${ENSEMBL_PATH}/misc-scripts/density_feature
-
-echo "Running repeat_coverage_calc.pl"
-echo "perl repeat_coverage_calc.pl -host $DB_HOST -user $DB_USER -port $DB_PORT -pass $DB_PASS -dbname $DB_NAME -mhost mysql-eg-pan-1.ebi.ac.uk -mport 4276 -muser ensro"
-
-perl repeat_coverage_calc.pl -host $DB_HOST -user $DB_USER -port $DB_PORT -pass $DB_PASS -dbname $DB_NAME -mhost mysql-eg-pan-1.ebi.ac.uk -mport 4276 -muser ensro
-
-
-# percent_gc_calc.pl
-
-cd ${ENSEMBL_PATH}/misc-scripts/density_feature
-
-echo "Running percent_gc_calc.pl"
-
-echo "perl percent_gc_calc.pl -host $DB_HOST -user $DB_USER -port $DB_PORT -pass $DB_PASS -dbname $DB_NAME -mhost mysql-eg-pan-1.ebi.ac.uk -mport 4276 -muser ensro"
-
-perl percent_gc_calc.pl -host $DB_HOST -user $DB_USER -port $DB_PORT -pass $DB_PASS -dbname $DB_NAME -mhost mysql-eg-pan-1.ebi.ac.uk -mport 4276 -muser ensro
-
-# meta_levels
-
-cd ${ENSEMBL_PATH}/misc-scripts
-
+# Set meta_levels
 echo "Setting the meta levels attributes"
 
-echo "perl meta_levels.pl -host $DB_HOST -user $DB_USER -pass $DB_PASS -port $DB_PORT -dbpattern $DB_NAME"
+cd ${ENSEMBL_PATH}/misc-scripts
+perl meta_levels.pl \
+     -host $DB_HOST -port $DB_PORT \
+     -user $DB_USER -pass $DB_PASS \
+     -dbpattern $DB_NAME
 
-perl meta_levels.pl -host $DB_HOST -user $DB_USER -pass $DB_PASS -port $DB_PORT -dbpattern $DB_NAME
-
-
-# update_meta_coord
+# Update meta_coord
+echo "Updating the meta_coord table"
 
 cd ${ENSEMBL_PATH}/misc-scripts/meta_coord
+perl update_meta_coord.pl \
+     -host $DB_HOST -port $DB_PORT \
+     -user $DB_USER -pass $DB_PASS \
+     -dbpattern $DB_NAME
 
-echo "Updating the meta_coord table"
-echo "perl update_meta_coord.pl -host $DB_HOST -user $DB_USER -pass $DB_PASS -port $DB_PORT -dbpattern $DB_NAME"
 
-perl update_meta_coord.pl -host $DB_HOST -user $DB_USER -pass $DB_PASS -port $DB_PORT -dbpattern $DB_NAME
-
-
-# overlapping_regions.pl script (add 'assembly.overlapping_regions' meta_key)
-cd ${ENSEMBL_PATH}/misc-scripts
-
+# Run overlapping_regions.pl (add 'assembly.overlapping_regions' meta_key)
 echo "Running overlapping_regions.pl"
 
-echo "perl overlapping_regions.pl -host $DB_HOST -user $DB_USER -pass $DB_PASS -port $DB_PORT -pattern "$DB_NAME" -interactive 0 --nolog"
-
-echo "y" | perl overlapping_regions.pl -host $DB_HOST -user $DB_USER -pass $DB_PASS -port $DB_PORT -pattern "$DB_NAME" -interactive 0 --nolog
+cd ${ENSEMBL_PATH}/misc-scripts
+echo "y" | perl overlapping_regions.pl \
+     -host $DB_HOST -port $DB_PORT \
+     -user $DB_USER -pass $DB_PASS \
+     -pattern "$DB_NAME" -interactive 0 --nolog
 
 
 # Remove pipeline tables - don't run that as we haven't run the protein pipelines yet
-
 # echo "final cleaning steps"
 
-# cat ${EG_FUNGI_PATH}/sql/final_cleaning_steps.sql | mysql -h $DB_HOST -u $DB_USER -P $DB_PORT -p$DB_PASS $DB_NAME
+# EG_PIPELINE_PATH=$EG_BASEDIR/release$EG_VERSION/ensembl_genomes/EG-pipelines
+# cat ${EG_PIPELINE_PATH}/sql/final_cleaning_steps.sql | mysql -h $DB_HOST -u $DB_USER -P $DB_PORT -p$DB_PASS $DB_NAME
 
 
-# optimization
-echo $PERL4LIB
-echo `which perl`
+# Optimization
 echo "Analyzing the tables"
 
 for t in `mysql -h $DB_HOST -u $DB_USER -P $DB_PORT -p$DB_PASS $DB_NAME -e "show tables"`
@@ -178,4 +111,3 @@ do
   echo "analyzing table, $t"
   mysql -h $DB_HOST -u $DB_USER -P $DB_PORT -p$DB_PASS $DB_NAME -e "analyze table $t"
 done
-
