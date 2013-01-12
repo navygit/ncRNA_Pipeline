9eb6728588d06572b1dfe76c2d151a9c0b4d1bb0
arnaud <>
2013-01-12 13:24:11 +0000

    fixed wrong password mysql parameter at the end of the wrapper

--- scripts/xrefs_pipeline/run_xrefs_pipeline.sh
+++ scripts/xrefs_pipeline/run_xrefs_pipeline.sh
@@ -129,6 +129,6 @@ fi
 
 echo "Deleting object_xref entries related to 'Uniprot_genename'..."
 
-mysql -h $DB_HOST -P $DB_PORT -u $DB_USER -p $DB_PASS -e "DELETE FROM object_xref WHERE xref_id IN (SELECT x.xref_id FROM xref x, external_db d WHERE x.external_db_id = d.external_db_id AND d.db_name = 'Uniprot_genename')"
+mysql -h $DB_HOST -P $DB_PORT -u $DB_USER -p$DB_PASS -e "DELETE FROM object_xref WHERE xref_id IN (SELECT x.xref_id FROM xref x, external_db d WHERE x.external_db_id = d.external_db_id AND d.db_name = 'Uniprot_genename')"
 
 echo "xrefs pipeline procedure has completed"
