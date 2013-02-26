bd4a5ebd294f749700d4c78b5090a10d57ec487c
arnaud <>
2013-02-26 13:58:24 +0000

    config directory structure for the est hive pipeline for the wheat unigene clusters vs brachypodium distachyon genome

---
+++ config/bdistachyon_wheat_unigene/Bio/EnsEMBL/Analysis/Config/General.pm
@@ -0,0 +1,42 @@
+package Bio::EnsEMBL::Analysis::Config::General;
+
+use strict;
+use vars qw(%Config);
+%Config = (
+    BIN_DIR  => '/nfs/panda/ensemblgenomes/external/bin',
+    DATA_DIR => '/nfs/panda/ensemblgenomes/external/data',
+    LIB_DIR  => '/nfs/panda/ensemblgenomes/external/lib',
+
+    # default target_file_directory
+    PIPELINE_TARGET_DIR => '/nfs/nobackup/ensemblgenomes/production/dna_pipelines/data/bdistachyon',
+    
+    #regex for spiting up slice input_ids
+    SLICE_INPUT_ID_REGEX => '(\S+)\.(\d+)-(\d+):?([^:]*)',	   
+    
+    ANALYSIS_WORK_DIR => '/nfs/nobackup/ensemblgenomes/production/dna_pipelines/data/bdistachyon',
+
+    ANALYSIS_REPEAT_MASKING => ['repeatmask'],
+
+    CORE_VERBOSITY   => 'WARNING',
+    LOGGER_VERBOSITY => 0,
+
+);
+
+  sub import {
+    my ($callpack) = caller(0);
+    my $pack = shift;
+    my @vars = @_ ? @_ : keys(%Config);
+    return unless @vars;
+    eval "package $callpack; use vars qw(".
+      join(' ', map { '$'.$_ } @vars) . ")";
+    die $@ if $@;
+    foreach (@vars) {
+      if (defined $Config{ $_ }) {
+        no strict 'refs';
+	*{"${callpack}::$_"} = \$Config{ $_ };
+      }else {
+	die "Error: Config: $_ not known\n";
+      }
+    }
+  }
+  1;
