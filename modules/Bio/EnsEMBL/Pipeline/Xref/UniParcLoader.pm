80a6f65332c2b25d4df03e82e096aa1fd142d6e3
dstaines <dstaines@ebi.ac.uk>
2013-08-23 10:01:17 +0100

    convert to using a package

---
+++ modules/Bio/EnsEMBL/Pipeline/Xref/UniParcLoader.pm
@@ -0,0 +1,93 @@
+package Bio::EnsEMBL::Pipeline::Xref::UniParcLoader;
+use Log::Log4perl qw/:easy/;
+use Bio::EnsEMBL::Utils::Argument qw( rearrange );
+
+sub new {
+  my ($proto, @args) = @_;
+  my $self = $proto->SUPER::new(@args);
+  ($self->{uniparc_dba}) = rearrange(['UNIPARC_DBA'], @args);
+  $self->{logger} = get_logger();
+  return $self;
+}
+
+sub logger {
+  my ($self) = @_;
+  return $self->{logger};
+}
+
+sub add_upis {
+  my ($self, $dba, $preserve_old) = @_;
+  if (!defined $preserve_old) {
+	$self->remove_upis($dba);
+  }
+  # now work through transcripts
+  my $translationN = 0;
+  my $upiN         = 0;
+  my $ddba         = $dba->get_DBEntryAdaptor();
+  $self->logger()->info("Processing translations for " . $dba->species());
+  my @translations = @{$dba->get_TranscriptAdaptor->fetch_all_by_biotype('protein_coding')};
+  for my $transcript (@translations) {
+	$translationN++;
+	$self->logger()->info("Processed $translationN/" . scalar @translations . " translations") if ($translationN % 1000 == 0);
+	$self->add_upi($ddba, $transcript->translation());
+  }
+  $self->logger()->info("Stored UPIs for $upiN of $translationN translations");
+  return;
+}
+
+sub remove_upis {
+  my ($self, $dba) = @_;
+  $self->logger()->info("Removing existing UniParc cross-references");
+  $dba->dbc()->sql_helper()->execute_update(
+	-SQL => q/
+		delete ox.* 
+		from object_xref ox,
+		xref x,
+		external_db d,
+		translation tl,
+		transcript tr,
+		seq_region sr,
+		coord_system cs
+		where
+		d.db_name='UniParc'
+		and d.external_db_id=x.external_db_id
+		and x.xref_id = ox.xref_id
+		and ox.ensembl_id = tl.translation_id
+		and ox.ensembl_object_type = 'Translation'
+		and tl.transcript_id=tr.transcript_id
+		and tr.seq_region_id=sr.seq_region_id
+		and sr.coord_system_id=cs.coord_system_id
+		and cs.species_id=?
+	/,
+	-PARAMS => [$dba->species_id()]);
+  return;
+} ## end sub remove_upis
+
+sub add_upi {
+  my ($self, $ddba, $translation) = @_;
+  $self->logger()->debug("Finding UPI for " . $translation->stable_id());
+  my $hash  = $self->md5_checksum($translation);
+  my @upis  = @{$uni_dba->dbc()->sql_helper->execute_simple(-SQL => q/select upi from uniparc.protein where md5=?/, -PARAMS => [$hash])};
+  my $nUpis = scalar(@upis);
+  if ($nUpis == 0) {
+	$self->logger()->warning("No UPI found for translation " . $translation->stable_id());
+  } elsif ($nUpis == 1) {
+	$upiN++;
+	$self->logger()->debug("UPI $upis[0] found for translation " . $translation->stable_id() . " - storing...");
+	$ddba->store(Bio::EnsEMBL::DBEntry->new(-PRIMARY_ID    => $upis[0],
+											-DISPLAY_LABEL => $upis[0],
+											-DBNAME        => 'UniParc'),
+				 $translation->dbID(),
+				 'Translation');
+  } else {
+	$self->logger()->warning("Multiple UPIs found for translation " . $translation->stable_id());
+  }
+  return;
+}
+
+sub md5_checksum {
+  my ($self, $sequence) = @_;
+  my $digest = Digest::MD5->new();
+  $digest->add($sequence->seq());
+  return uc($digest->hexdigest());
+}
--- scripts/xrefs_pipeline/load_uniparc_xrefs.pl
+++ scripts/xrefs_pipeline/load_uniparc_xrefs.pl
@@ -3,11 +3,9 @@ use warnings;
 use strict;
 
 use Bio::EnsEMBL::Utils::CliHelper;
-use Bio::EnsEMBL::DBEntry;
-use Bio::EnsEMBL::Analysis;
-use Digest::MD5;
 use Log::Log4perl qw/:easy/;
 use Pod::Usage;
+use Bio::EnsEMBL::Pipeline::Xref::UniParcLoader;
 
 my $cli_helper = Bio::EnsEMBL::Utils::CliHelper->new();
 # get the basic options for connecting to a database server
@@ -27,68 +25,11 @@ my $logger = get_logger();
 $logger->info("Connecting to UniParc database");
 my ($uni_dba) = @{$cli_helper->get_dbas_for_opts($opts, 1, 'uniparc')};
 
+my $loader = Bio::EnsEMBL::Pipeline::Xref::UniParcLoader->new(-UNIPARC_DBA=>$uni_dba);
+
 $logger->info("Connecting to core database(s)");
 for my $core_dba_details (@{$cli_helper->get_dba_args_for_opts($opts)}) {
   my $dba = Bio::EnsEMBL::DBSQL::DBAdaptor->new(%{$core_dba_details});
   $logger->info("Processing " . $dba->species());
-  # clear out original xrefs first
-  $logger->info("Removing existing UniParc cross-references");
-  $dba->dbc()->sql_helper()->execute_update(
-	-SQL => q/
-		delete ox.* 
-		from object_xref ox,
-		xref x,
-		external_db d,
-		translation tl,
-		transcript tr,
-		seq_region sr,
-		coord_system cs
-		where
-		d.db_name='UniParc'
-		and d.external_db_id=x.external_db_id
-		and x.xref_id = ox.xref_id
-		and ox.ensembl_id = tl.translation_id
-		and ox.ensembl_object_type = 'Translation'
-		and tl.transcript_id=tr.transcript_id
-		and tr.seq_region_id=sr.seq_region_id
-		and sr.coord_system_id=cs.coord_system_id
-		and cs.species_id=?
-	/,
-	-PARAMS => [$dba->species_id()]);
-  # now work through transcripts
-  my $translationN = 0;
-  my $upiN         = 0;
-  my $ddba         = $dba->get_DBEntryAdaptor();
-  $logger->info("Processing translations for " . $dba->species());
-  my @translations = @{$dba->get_TranscriptAdaptor->fetch_all_by_biotype('protein_coding')};
-  for my $transcript (@translations) {
-	$translationN++;
-	$logger->info("Processed $translationN/" . scalar @translations . " translations") if ($translationN % 100 == 0);
-	my $translation = $transcript->translation();
-	$logger->debug("Finding UPI for " . $translation->stable_id());
-	my $hash  = md5_checksum($translation);
-	my @upis  = @{$uni_dba->dbc()->sql_helper->execute_simple(-SQL => q/select upi from uniparc.protein where md5=?/, -PARAMS => [$hash])};
-	my $nUpis = scalar(@upis);
-	if ($nUpis == 0) {
-	  $logger->warning("No UPI found for translation " . $translation->stable_id());
-	} elsif ($nUpis == 1) {
-	  $upiN++;
-	  $logger->debug("UPI $upis[0] found for translation " . $translation->stable_id() . " - storing...");
-	  $ddba->store(Bio::EnsEMBL::DBEntry->new(-PRIMARY_ID    => $upis[0],
-											  -DISPLAY_LABEL => $upis[0],
-											  -DBNAME        => 'UniParc'),
-				   $translation->dbID(),
-				   'Translation');
-	} else {
-	  $logger->warning("Multiple UPIs found for translation " . $translation->stable_id());
-	}
-  }
-  $logger->info("Stored UPIs for $upiN of $translationN translations");
+  $loader->add_upis($dba);
 } ## end for my $core_dba_details...
-
-sub md5_checksum {
-  my ($sequence) = @_;
-  my $digest = Digest::MD5->new();
-  $digest->add($sequence->seq());
-  return uc($digest->hexdigest());
-}
